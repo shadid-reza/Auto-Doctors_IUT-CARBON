0967c015a13b2936bb3d272f736d4326b262cb31
H.M.Shadid Reza Chowdhury <hmshadidreza@gmail.com>
2022-02-27 16:34:59 +0600

    back button login page added

--- Auto_doctors/src/auto_doctors/Login_pageController.java
+++ Auto_doctors/src/auto_doctors/Login_pageController.java
@@ -4,9 +4,16 @@
  */
 package auto_doctors;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -23,4 +30,18 @@ public class Login_pageController implements Initializable {
         // TODO
     }    
     
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    
+     public void HandleBackBtn (ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/auto_doctors/Home_page.fxml"));
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
 }
