aae6236b74b70df99599a38d54a0c9cf55ea4558
H.M.Shadid Reza Chowdhury <hmshadidreza@gmail.com>
2022-03-23 16:36:06 +0600

    it works

--- Auto_doctors/src/auto_doctors/Appoinment_pageController.java
+++ Auto_doctors/src/auto_doctors/Appoinment_pageController.java
@@ -4,9 +4,21 @@
  */
 package auto_doctors;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -18,9 +30,47 @@ public class Appoinment_pageController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+     @FXML
+    private AnchorPane slider;
+     
+     @FXML
+    private Label Menu;
+
+    @FXML
+    private Label MenuBack;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        
+        slider.setTranslateX(-200);
+        Menu.setOnMouseClicked(event-> {
+                TranslateTransition slide = new TranslateTransition();
+                slide.setDuration(Duration.seconds(0.4));
+                slide.setNode(slider);
+                slide.setToX(0);
+                slide.play();
+                slider.setTranslateX(-200);
+                slide.setOnFinished((ActionEvent e)->{
+                Menu.setVisible(false);
+                MenuBack.setVisible(true);
+            }); 
+        });
+        MenuBack.setOnMouseClicked(event-> {
+                TranslateTransition slide = new TranslateTransition();
+                slide.setDuration(Duration.seconds(0.4));
+                slide.setNode(slider);
+                slide.setToX(-200);
+                slide.play();
+                slider.setTranslateX(0);
+                slide.setOnFinished((ActionEvent e)->{
+                Menu.setVisible(true);
+                MenuBack.setVisible(false);
+            }); 
+        });
+       
     }    
     
 }
